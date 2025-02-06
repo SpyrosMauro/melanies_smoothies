faa35bcba3da97b24d42547f1b0b0bc0e2a3f957
Spyros Mavroforos <spyrosmauro@gmail.com>
2025-02-06 17:06:32 +0100

    Update streamlit_app.py
    add search options

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,9 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+#have is as pandas
+pd_df = my_dataframe.to_pandas()
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
@@ -41,7 +44,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get(websiteToUse + fruit_chosen)
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        smoothiefroot_response = requests.get(websiteToUse + search_on)
         sf_df_A = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     ingredients_string = ingredients_string[:-1]
     #st.write(ingredients_string)
